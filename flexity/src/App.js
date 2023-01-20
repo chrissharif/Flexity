a93ede6d20eeea447e64e7915d324e36bfe39da5
Christopher Sharif <christophersharif@gmail.com>
2023-01-20 13:16:55 -0500

    Added All Files From Old File

--- flexity/src/App.js
+++ flexity/src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="body">
+      <div className='title'>Flexity</div>
+      <header className="">
+        
       </header>
+      <div className='category-container'>
+        <div className='category'>
+          <header className='category-title'>Jobs</header>
+          <ul className='list'>
+            <li className='list-item'>Accounting</li>
+            <li className='list-item'>Administration</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Architecture</li>
+            <li className='list-item'>Business</li>
+            <li className='list-item'>Customer Service</li>
+            <li className='list-item'>Education</li>
+            <li className='list-item'>Food</li>
+            <li className='list-item'>Government</li>
+            <li className='list-item'>Human Resources</li>
+            <li className='list-item'>Marketing</li>
+            <li className='list-item'>Medical</li>
+            <li className='list-item'>Nonprofit</li>
+            <li className='list-item'>Real Estate</li>
+            <li className='list-item'>Retail</li>
+            <li className='list-item'>Sales</li>
+            <li className='list-item'>Salon</li>
+            <li className='list-item'>Security</li>
+            <li className='list-item'>Software</li>
+            <li className='list-item'>Web Design</li>
+          </ul>
+        </div>
+        <div className='category'>
+          <header className='category-title'>Marketplace</header>
+          <ul className='list'>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+          </ul>
+        </div>
+        <div className='category'>
+          <header className='category-title'>Freelance</header>
+          <ul className='list'>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+          </ul>
+        </div>
+        <div className='category'>
+          <header className='category-title'>Real Estate</header>
+          <ul className='list'>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+            <li className='list-item'>Agriculture</li>
+          </ul>
+        </div>
+      </div>
+      <footer className='footer'>
+        Created by Christopher Sharif
+        <div className='socials'>
+          <a href='https://github.com/chrissharif' target='_blank' rel='noreferer'>
+            <img src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt='My GitHub' className='social'/>
+          </a>
+
+            <a href='https://www.linkedin.com/in/christopher-sharif-754598211/' target='_blank' rel='noreferer'>
+              <img src='https://www.iconpacks.net/icons/1/free-linkedin-icon-112-thumb.png' alt='My LinkedIn' className='social'/>
+            </a>
+        </div>
+      </footer>
     </div>
   );
 }
